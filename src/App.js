9c4f029d718df0a4a9187f4358f046e51d95d90d
kwak-bs <iam-bs@naver.com>
2022-03-03 17:25:37 +0900

    Refactor : module

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Image from "./components/Image";
 import Introduce from "./components/Introduce";
 
 import {nephew1,nephew2,nephew3,nephew4,nephew5,nephew6,nephew7,nephew8} from "./assets/index";
+import { introduceList } from './assets/introduceList';
 import './App.css';
 
 export default function App() {
@@ -12,22 +13,6 @@ export default function App() {
 
   const title = "우리 요한이를 소개합니다."
   const imageList = [nephew1,nephew2,nephew3,nephew4,nephew5,nephew6,nephew7,nephew8];
-  const introduceList = [
-    `요한이는 뺑슨의 조카 입니다.\n 
-  지난 1월에 돌이 지난 꼬맹이에요. \n 
-  치명적인 매력을 가진 우리 요한이를 소개합니다~~~` , 
-  `이 사진은 태어난지 6개월도 되지 않았을 때, 아기 수영장에서 찍은 사진이에요! \n
-  목 튜브를 하는 모습이 너무 귀여워서 찍었답니다.`, 
-`이 사진은 볼이 너무 빵빵한게 귀여워서 가져왔습니다. \n
-아 참! 간단하게 요한이 소개좀 할게요~~ \n
-이름 : 우요한\n
-성별 : 남자\n
-생년원일 : 2021.01.22`, 
-`때로는 상남자!`, 
-`가끔은 개구장이~`,
-`그리고 이쁘기도하고`,
-`스타벅스도 잘어울려요~~`,
-`이제는 어엿하게 어린이집도 다니는 2살 우요한이었습니다~~`]
 
   const handleClick = () => {
     if(index === 8) {
